a22e5a35602872b5f9a806c97b1caa3ee537bba5
sjwang <sjwang0719@gmail.com>
2018-04-21 17:17:54 +0800

    LinkedList

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,27 @@
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        if (head == null) return false;
+        while (fast != null && slow != null) {
+            if (fast.next == null) break;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
