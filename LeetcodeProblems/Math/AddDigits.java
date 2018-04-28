d6f7af9f4c1ec2dc86aa2496672f186263ea29e8
sjwang <sjwang0719@gmail.com>
2018-04-28 10:18:34 +0800

    pro258

---
+++ LeetcodeProblems/Math/AddDigits.java
@@ -0,0 +1,28 @@
+package Math;
+
+/**
+ * Created by sjwang on 28/04/2018.
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *
+ * For example:
+ *
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ *
+ */
+
+public class AddDigits {
+    public int addDigits(int num) {
+        int sum = 0, temp = num;
+        if(num / 10 == 0) return num;
+        while(temp != 0){
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return addDigits(sum);
+    }
+
+    public static void main(String args[]){
+        System.out.println(new AddDigits().addDigits(38));
+    }
+
+}
