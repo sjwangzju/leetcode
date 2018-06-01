3047426e96b55437816ddfbaeb228da6aa21c5e1
sjwang <sjwang0719@gmail.com>
2018-06-01 22:49:34 +0800

    pro728

--- LeetcodeProblems/Math/SelfDividingNumbers.java
+++ LeetcodeProblems/Math/SelfDividingNumbers.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by sjwang on 27/04/2018.
+ * Created by sjwang on 04/27/2018.
  * A self-dividing number is a number that is divisible by every digit it contains.
  *
  * For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
