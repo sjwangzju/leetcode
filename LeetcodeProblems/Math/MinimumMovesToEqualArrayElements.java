6e76bf345afd5766568b412f78584c2ae40edd9b
sjwang <sjwang0719@gmail.com>
2018-06-02 21:58:24 +0800

    pro453

--- LeetcodeProblems/Math/MinimumMovesToEqualArrayElements.java
+++ LeetcodeProblems/Math/MinimumMovesToEqualArrayElements.java
@@ -1,7 +1,7 @@
 package Math;
 
 /**
- * Created by sjwang on 28/04/2018.
+ * Created by sjwang on 04/28/2018.
  * Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
  *
  * Example:
