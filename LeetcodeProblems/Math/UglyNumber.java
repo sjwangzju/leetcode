79885c12b05db658641c59622c5c64c9ed72469a
sjwang <sjwang0719@gmail.com>
2018-06-08 21:46:22 +0800

    pro263

--- LeetcodeProblems/Math/UglyNumber.java
+++ LeetcodeProblems/Math/UglyNumber.java
@@ -1,7 +1,7 @@
 package Math;
 
 /**
- * Created by sjwang on 29/04/2018.
+ * Created by sjwang on 04/29/2018.
  * Write a program to check whether a given number is an ugly number.
  *
  * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
