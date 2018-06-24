2cd09ffe72ce1cf663ac64f1d84fe46a06d9807c
sjwang <sjwang0719@gmail.com>
2018-06-24 16:00:18 +0800

    pro771

--- LeetcodeProblems/HashTable/JewelsAndStones.java
+++ LeetcodeProblems/HashTable/JewelsAndStones.java
@@ -2,7 +2,7 @@ package HashTable;
 import java.util.Hashtable;
 
 /**
- * Created by sjwang on 24/04/2018.
+ * Created by sjwang on 04/24/2018.
  * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
  * Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
  *
