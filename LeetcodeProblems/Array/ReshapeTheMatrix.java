9311819227b426863a5b55c0d619b22275f8e3c0
sjwang <sjwang0719@gmail.com>
2018-07-09 16:16:59 +0800

    pro566

--- LeetcodeProblems/Array/ReshapeTheMatrix.java
+++ LeetcodeProblems/Array/ReshapeTheMatrix.java
@@ -1,7 +1,7 @@
 package Array;
 
 /**
- * Created by sjwang on 12/05/2018.
+ * Created by sjwang on 05/12/2018.
  * In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
  *
  * You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
