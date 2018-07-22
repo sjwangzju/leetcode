ffab4da4b1eef81d1df00c8fbc1e5dfcb9f5f82f
sjwang <sjwang0719@gmail.com>
2018-07-22 20:59:50 +0800

    pro152

---
+++ LeetcodeProblems/Array/MaximumProductSubarray.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sjwang on 07/22/2018.
+ * Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+ *
+ * Example 1:
+ * Input: [2,3,-2,4]
+ * Output: 6
+ * Explanation: [2,3] has the largest product 6.
+ *
+ * Example 2:
+ * Input: [-2,0,-1]
+ * Output: 0
+ * Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ */
+public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        if(nums.length == 0 || nums == null) return 0;
+        int max = nums[0], min = nums[0], res = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+    public static void main(String args[]){
+        int[] nums = {2,3,-3,-5,1,19};
+        System.out.println(new MaximumProductSubarray().maxProduct(nums));
+    }
+}
