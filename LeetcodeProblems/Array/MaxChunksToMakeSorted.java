31d4127d03267c801f25d1971ab644be8488bbc0
sjwang <sjwang0719@gmail.com>
2018-08-15 23:23:21 -0700

    pro769

--- LeetcodeProblems/Array/MaxChunksToMakeSorted.java
+++ LeetcodeProblems/Array/MaxChunksToMakeSorted.java
@@ -2,7 +2,9 @@ package Array;
 
 /**
  * Created by sjwang on 07/13/2018.
- * Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions), and individually sort each chunk.  After concatenating them, the result equals the sorted array.
+ *
+ * Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions),
+ * and individually sort each chunk.  After concatenating them, the result equals the sorted array.
  *
  * What is the most number of chunks we could have made?
  *
