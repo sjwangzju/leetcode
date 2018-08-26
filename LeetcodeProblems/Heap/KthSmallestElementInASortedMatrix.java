b0ab82a07ad1a429d8f1a19454bb30a9b17af966
sjwang <sjwang0719@gmail.com>
2018-08-25 23:52:55 -0700

    pro378

--- LeetcodeProblems/Heap/KthSmallestElementInASortedMatrix.java
+++ LeetcodeProblems/Heap/KthSmallestElementInASortedMatrix.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 /**
  * Created by sjwang on 07/15/2018.
- * Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
  *
+ * Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
  * Note that it is the kth smallest element in the sorted order, not the kth distinct element.
  *
  * Example:
