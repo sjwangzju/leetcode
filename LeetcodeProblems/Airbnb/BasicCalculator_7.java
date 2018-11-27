40aa4a6dd1af92c8570da3854ae896e2d9950b52
sjwang <sjwang0719@gmail.com>
2018-11-26 17:27:33 -0800

    AB_7

---
+++ LeetcodeProblems/Airbnb/BasicCalculator_7.java
@@ -0,0 +1,100 @@
+package Airbnb;
+
+import java.util.Stack;
+
+/**
+ * similar to lc224, lc227
+ *
+ * s = s.replaceAll("\\s", "") -- remove all the spaces and tabs in string s
+ */
+public class BasicCalculator_7 {
+
+    /**
+     * lc224 -- calculator with ( ) + - and positive numbers
+     * @param input
+     * @return
+     */
+    public int calculatorI(String input) {
+        input = input.replaceAll("\\s", "");
+        Stack<Integer> stack = new Stack<>();
+        int number = 0;
+        int res = 0;
+        int sign = 1;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isDigit(ch)) {
+                number = number * 10 + ch - '0';
+            }
+            if (ch == '+') {
+                res += sign * number;
+                number = 0;
+                sign = 1;
+            }
+            if (ch == '-') {
+                res += sign * number;
+                number = 0;
+                sign = -1;
+            }
+            if (ch == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            }
+            if (ch == ')') {
+                res += sign * number;
+                number = 0;
+                res *= stack.pop();
+                res += stack.pop();
+            }
+        }
+        if (number != 0) {
+            res += sign * number;
+        }
+        return res;
+    }
+
+    /**
+     * lc227 -- calculator with + - * / and positive numbers
+     * @param input
+     * @return
+     */
+    public int calculatorII(String input) {
+        input = input.replaceAll("\\s", "");
+        Stack<Integer> stack = new Stack<>();
+        char sign = '+';
+        int res = 0;
+        int number = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                number = input.charAt(i) - '0';
+                while (i + 1 < input.length() && Character.isDigit(input.charAt(i + 1))) {
+                    number = number * 10 + input.charAt(i + 1) - '0';
+                    i++;
+                }
+            }
+            if (!Character.isDigit(input.charAt(i)) || i == input.length() - 1) {
+                if (sign == '+') stack.push(number);
+                if (sign == '-') stack.push(-number);
+                if (sign == '*') stack.push(stack.pop() * number);
+                if (sign == '/') stack.push(stack.pop() / number);
+                sign = input.charAt(i);
+                number = 0;
+            }
+        }
+
+        while (!stack.empty()) {
+            res += stack.pop();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String input1 = " 5 +   6 - (12+ 2 )- 1   ";
+        String input2 = "3 - 5 * 2 / 3 +  1";
+        int res = new BasicCalculator_7().calculatorI(input1);
+        System.out.println(res);
+    }
+}
