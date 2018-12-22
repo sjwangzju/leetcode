a3adfc98dd825d66d6d33c7d8ee97c226c33689f
sjwang <sjwang0719@gmail.com>
2018-12-22 12:25:39 -0800

    topological sort

--- LeetcodeProblems/FB/CourseScheduler.java
+++ LeetcodeProblems/FB/CourseScheduler.java
@@ -12,12 +12,12 @@ public class CourseScheduler {
      * @param prerequisites
      * @return
      */
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (prerequisites.length == 0 || prerequisites == null) return true;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+        int[] ret = new int[numCourses];
 
         Map<Integer, Set<Integer>> map = new HashMap<>();
         Map<Integer, Integer> indegree = new HashMap<>();
-        Map<Integer, Integer> parent = new HashMap<>();
 
         for (int i = 0; i < numCourses; i++) {
             indegree.put(i, 0);
@@ -27,12 +27,11 @@ public class CourseScheduler {
             for (int i = 0; i < n.length - 1; i++) {
                 int c1 = n[i];
                 int c2 = n[i + 1];
-                Set<Integer> s = map.getOrDefault(c1, new HashSet<>());
-                if (!s.contains(c2)) {
-                    s.add(c2);
-                    map.put(c1, s);
-                    indegree.put(c2, indegree.getOrDefault(c2, 0) + 1);
-                    parent.put(c2, c1);
+                Set<Integer> s = map.getOrDefault(c2, new HashSet<>());
+                if (!s.contains(c1)) {
+                    s.add(c1);
+                    map.put(c2, s);
+                    indegree.put(c1, indegree.getOrDefault(c1, 0) + 1);
                 }
             }
         }
@@ -47,8 +46,8 @@ public class CourseScheduler {
         int cnt = 0;
         while (!queue.isEmpty()) {
             int tmp = queue.poll();
+            ret[cnt] = tmp;
             cnt++;
-            System.out.println(tmp);
             if (map.containsKey(tmp)) {
                 for (int n: map.get(tmp)) {
                     indegree.put(n, indegree.get(n) - 1);
@@ -58,12 +57,19 @@ public class CourseScheduler {
                 }
             }
         }
-        return cnt == numCourses;
+        if (cnt == numCourses) {
+            return ret;
+        }
+        return new int[0];
     }
 
     public static void main(String[] args) {
-        int numCourses = 6;
-        int[][] prerequisites = {{1,0},{1,3},{3,2},{3,4}};
-        System.out.println(new CourseScheduler().canFinish(numCourses, prerequisites));
+        int numCourses = 5;
+        int[][] prerequisites = {{1,0},{0,1}};
+//        System.out.println(new CourseScheduler().canFinish(numCourses, prerequisites));
+        int[] res = new CourseScheduler().findOrder(numCourses, prerequisites);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
     }
 }
