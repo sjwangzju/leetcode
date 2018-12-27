aaf445bacb1bd427bf7bce556dac96e1d7774008
sjwang <sjwang0719@gmail.com>
2018-12-26 20:19:49 -0800

    bfs

--- LeetcodeProblems/Airbnb/CheapestFlightWithinKStops_25.java
+++ LeetcodeProblems/Airbnb/CheapestFlightWithinKStops_25.java
@@ -4,82 +4,69 @@ import java.util.*;
 
 public class CheapestFlightWithinKStops_25 {
 
-    /**
-     * find the cheapest price
-     *
-     * Dijkstra --  continually search the next node with the lowest cost
-     *
-     * @param n
-     * @param flights
-     * @param src
-     * @param dst
-     * @param K
-     * @return
-     */
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        // key: src, value: dst, price
-        Map<Integer, Map<Integer, Integer>> prices = new HashMap<>();
+    public int findCheapestPrice(int n, int[][] edges, int src, int dst, int k) {
+        Map<Integer, Map<Integer,Integer>> routes = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
         Map<Integer, Integer> parent = new HashMap<>();
         Map<Integer, Integer> cost = new HashMap<>();
-        Set<Integer> cities = new HashSet<>();
 
-        for (int[] flight: flights) {
-            Map<Integer, Integer> map = prices.getOrDefault(flight[0], new HashMap<>());
-            map.put(flight[1], flight[2]);
-            cities.add(flight[0]);
-            cities.add(flight[1]);
-            prices.put(flight[0], map);
+        for(int[] edge: edges) {
+            Map<Integer,Integer> route = routes.getOrDefault(edge[0], new HashMap<>());
+            route.put(edge[1], edge[2]);
+            routes.put(edge[0], route);
         }
 
-        for (int i: cities) {
-            if (i == src) {
-                cost.put(i, 0);
-            } else {
-                cost.put(i, Integer.MAX_VALUE);
-            }
-        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
+        int[] cur = new int[]{0, src, k + 1};
+        pq.offer(cur);
 
-        // int[] a -> a[0] accumulated price, a[1] current city, a[2] parent of curCity, a[3] remain stops
-        Queue<int[]> q = new PriorityQueue<>((a, b) -> cost.get(a[0]) - cost.get(b[0]));
-        q.offer(new int[]{src, K});
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curCity = cur[0];
-            int curStopsLeft = cur[1];
+        while(!pq.isEmpty()) {
+            int[] tmp = pq.poll();
+            int curCost = tmp[0];
+            int curCity = tmp[1];
+            int stopsLeft = tmp[2];
 
             if (curCity == dst) {
-                outputRoute(curCity, src, parent);
-                return cost.get(curCity);
+                 printPath(parent, curCity, src);
+                return curCost;
             }
-            if (curStopsLeft > 0) {
-                Map<Integer, Integer> adj = prices.getOrDefault(curCity, new HashMap<>());
-                for (int k: adj.keySet()) {
-                    int last = cost.getOrDefault(k, Integer.MAX_VALUE);
-                    if (cost.get(curCity) + adj.get(k) < last) {
-                        parent.put(k, curCity);
-                        q.offer(new int[]{k, curStopsLeft - 1});
-                        cost.put(k, cost.get(curCity) + adj.get(k));
-                    }
+
+            if (stopsLeft > 0) {
+                Map<Integer,Integer> adj = routes.getOrDefault(curCity, new HashMap<>());
+                for (int next: adj.keySet()) {
+                    int nextCost = curCost + adj.get(next);
+
+                    pq.offer(new int[]{nextCost, next, stopsLeft - 1});
+                    parent.put(next, curCity);
+                    cost.put(next, nextCost);
+
                 }
             }
         }
+
         return -1;
     }
 
-    // output the optimal route
-    public void outputRoute(int city, int src, Map<Integer, Integer> parent) {
+    public static void printPath(Map<Integer, Integer> parent, int city, int src) {
+        int tmp = city;
         StringBuilder sb = new StringBuilder();
-        while (city != src) {
-            sb.insert(0, "->" + city);
-            city = parent.get(city);
+        sb.append(tmp);
+        while(tmp != src) {
+            tmp = parent.get(tmp);
+            sb.insert(0, tmp + "->");
         }
-        sb.insert(0, "the optimal flight route: " + city);
         System.out.println(sb.toString());
+        return;
     }
 
     public static void main(String[] args) {
-        int[][] flights = {{0,1,100}, {1,2,100}, {0,2,500}};
-        int cheap = new CheapestFlightWithinKStops_25().findCheapestPrice(3, flights, 0, 2, 3);
-        System.out.println("cheapest cost: " + cheap);
+        int[][] edges = {{0,1,5},{1,2,5},{0,3,2},{3,1,2},{1,4,1},{4,2,1}};
+        int n = 5;
+        int src = 0;
+        int dst = 2;
+        int k = 2;
+
+        int res = new CheapestFlightWithinKStops_25().findCheapestPrice(n, edges, src, dst, k);
+        System.out.println(res);
     }
 }
