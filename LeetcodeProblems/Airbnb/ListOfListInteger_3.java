a79dc86ebf6bf852137454fe5a033e9c772b97d4
sjwang <sjwang0719@gmail.com>
2018-12-28 21:08:43 -0800

    iterator

--- LeetcodeProblems/Airbnb/ListOfListInteger_3.java
+++ LeetcodeProblems/Airbnb/ListOfListInteger_3.java
@@ -1,47 +1,74 @@
 package Airbnb;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ListOfListInteger_3 implements Iterator<Integer> {
 
-    private Iterator<List<Integer>> rowIterator;
-    private Iterator<Integer> colIterator;
+//    private Iterator<List<Integer>> rowIterator;
+//    private Iterator<Integer> colIterator;
+//
+//    public ListOfListInteger_3(List<List<Integer>> list) {
+//        this.rowIterator = list.iterator();
+//        this.colIterator = Collections.emptyIterator();
+//    }
+//
+//    @Override
+//    public boolean hasNext() {
+//        while((colIterator == null || !colIterator.hasNext()) && rowIterator.hasNext()) {
+//            colIterator = rowIterator.next().iterator();
+//        }
+//        return colIterator != null && colIterator.hasNext();
+//    }
+//
+//    @Override
+//    public Integer next() {
+//        return colIterator.next();
+//    }
+//
+//    @Override
+//    public void remove() {
+//        while((colIterator == null || !colIterator.hasNext()) && rowIterator.hasNext()) {
+//            colIterator = rowIterator.next().iterator();
+//        }
+//        if (colIterator != null) {
+//            colIterator.remove();
+//        }
+//    }
 
-    public ListOfListInteger_3(List<List<Integer>> list) {
-        this.rowIterator = list.iterator();
-        this.colIterator = Collections.emptyIterator();
+    Iterator<List<Integer>> list;
+    Iterator<Integer> num;
+
+    public ListOfListInteger_3(List<List<Integer>> input){
+        if (input == null || input.size() == 0) return;
+        list = input.iterator();
+        num = list.next().iterator();
+        // getNextNum();
     }
 
-    @Override
+    public void getNextNum(){
+        while (!num.hasNext() && list.hasNext()) {
+            num = list.next().iterator();
+        }
+    }
+
     public boolean hasNext() {
-        while((colIterator == null || !colIterator.hasNext()) && rowIterator.hasNext()) {
-            colIterator = rowIterator.next().iterator();
-        }
-        return colIterator != null && colIterator.hasNext();
+        getNextNum();
+        return num != null && num.hasNext();
     }
 
-    @Override
-    public Integer next() {
-        return colIterator.next();
+    public Integer next(){
+        return num.next();
     }
 
-    @Override
-    public void remove() {
-        while((colIterator == null || !colIterator.hasNext()) && rowIterator.hasNext()) {
-            colIterator = rowIterator.next().iterator();
-        }
-        if (colIterator != null) {
-            colIterator.remove();
-        }
+    public void remove(){
+        num.remove();
+        return;
     }
 
     public static void main(String[] args) {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> l1= new ArrayList<>();
-        l1.add(1); l1.add(2); l1.add(null);
+        l1.add(1); l1.add(2); l1.add(3);
         List<Integer> l2= new ArrayList<>();
         l2.add(3);
         List<Integer> l3= new ArrayList<>();
@@ -51,10 +78,10 @@ public class ListOfListInteger_3 implements Iterator<Integer> {
         ListOfListInteger_3 l = new ListOfListInteger_3(list);
         System.out.println(l.hasNext());
         System.out.println(l.next());
-        //l.remove();
+        l.remove();
         System.out.println(l.hasNext());
         System.out.println(l.next());
-        //l.remove();
+        l.remove();
         System.out.println(l.hasNext());
         System.out.println(l.next());
         System.out.println(l.hasNext());
