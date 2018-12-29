86ffc410992a48a10a453b6ce8ec53deae956156
sjwang <sjwang0719@gmail.com>
2018-12-28 21:09:00 -0800

    back tracking

--- LeetcodeProblems/Airbnb/MenuCombinationSum_22.java
+++ LeetcodeProblems/Airbnb/MenuCombinationSum_22.java
@@ -8,41 +8,41 @@ import java.util.List;
  * get the centPrice (price * 100) to avoid comparing two doubles
  */
 public class MenuCombinationSum_22 {
-    public List<List<Double>> MenuCombination(double[] prices, double target) {
+
+    public static List<List<Double>> MenuCombination(double[] prices, double target) {
         List<List<Double>> res = new ArrayList<>();
         List<Double> cur = new ArrayList<>();
-        if (prices == null || prices.length == 0 || target < 0) return res;
-
-        int centTarget = (int)(target * 100);
         int[] centPrices = new int[prices.length];
-        boolean[] used = new boolean[prices.length];
 
         Arrays.sort(prices);
+
         for (int i = 0; i < prices.length; i++) {
-            centPrices[i] = (int)(prices[i] * 100);
+            centPrices[i] = (int)prices[i] * 100;
         }
-        findCombination(res, cur, prices, centTarget, centPrices, 0, used);
+
+        find(res, cur, prices, centPrices, (int)target * 100, 0);
+
         return res;
     }
 
-    public void findCombination(List<List<Double>> res, List<Double> cur,
-                                double[] prices, int centTarget, int[] centPrices, int start, boolean[] used) {
-        if (centTarget == 0) {
+
+    public static void find(List<List<Double>> res, List<Double> cur, double[] prices,
+                            int[] centPrices, int target, int start) {
+        if (target == 0) {
             res.add(new ArrayList<>(cur));
             return;
         }
-        for (int i = start; i < centPrices.length; i++) {
-            if (i > 0 && centPrices[i] == centPrices[i - 1] && !used[i - 1]
-                    || cur.size() > 0 && prices[i] < cur.get(cur.size() - 1)) continue;
 
-            // break when this combination doesn't work
-            if (centTarget < centPrices[i]) break;
+        if (target < 0) return;
+
+        for (int i = start; i < centPrices.length; i++) {
+            if (i > start && centPrices[i] == centPrices[i - 1]) continue;
             cur.add(prices[i]);
-            used[i] = true;
-            findCombination(res, cur, prices, centTarget - centPrices[i], centPrices, start + 1, used);
+            find(res, cur, prices, centPrices, target - centPrices[i], i + 1);
             cur.remove(cur.size() - 1);
-            used[i] = false;
         }
+
+        return;
     }
 
     public static void main(String[] args) {
