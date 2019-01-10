37dacb98136b700b5f846d679e7fb5b70d3585c0
sjwang <sjwang0719@gmail.com>
2019-01-09 22:57:39 -0800

    merge

---
+++ LeetcodeProblems/FB/LC88MergeSortedArray.java
@@ -0,0 +1,26 @@
+package FB;
+
+public class LC88MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            nums1[k--] = nums1[i] >= nums2[j] ? nums1[i--] : nums2[j--];
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        new LC88MergeSortedArray().merge(nums1, m, nums2, n);
+        for (int num: nums1) {
+            System.out.println(num);
+        }
+    }
+}
