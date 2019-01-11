4a276e2274b7b325a72141d8971599f7d853d742
sjwang <sjwang0719@gmail.com>
2019-01-10 16:53:37 -0800

    binary

--- LeetcodeProblems/FB/LC67AddBinary.java
+++ LeetcodeProblems/FB/LC67AddBinary.java
@@ -3,34 +3,18 @@ package FB;
 public class LC67AddBinary {
     public String addBinary(String a, String b) {
         StringBuilder res = new StringBuilder();
-        if (a.length() < b.length()) {
-            String tmp = b;
-            b = a;
-            a = tmp;
-        }
-        int len1 = a.length();
-        int len2 = b.length();
+        int n1 = a.length() - 1;
+        int n2 = b.length() - 1;
         int add = 0;
-        for (int i = 0; i < len1; i++) {
-            int n1 = a.charAt(len1 - 1 - i) - '0';
-            int n2;
-            if (len2 - 1 - i < 0) {
-                n2 = 0;
-            } else {
-                n2 = b.charAt(len2 - 1 - i) - '0';
-            }
-            int cur = n1 + n2 + add;
-            add = 0;
-            if (cur >= 2) {
-                cur -= 2;
-                add = 1;
-            }
-            res.insert(0, cur);
+        while (n1 >= 0 || n2 >= 0) {
+            int sum = add;
+            if (n1 >= 0) sum += a.charAt(n1--) - '0';
+            if (n2 >= 0) sum += b.charAt(n2--) - '0';
+            res.append(sum % 2);
+            add = sum / 2;
         }
-        if (add == 1) {
-            res.insert(0, 1);
-        }
-        return res.toString();
+        if (add != 0) res.append(add);
+        return res.reverse().toString();
     }
 
     public static void main(String[] args) {
