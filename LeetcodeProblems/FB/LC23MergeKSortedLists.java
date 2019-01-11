1353294fbf566a762da0adbc19f9586413393510
sjwang <sjwang0719@gmail.com>
2019-01-10 20:18:25 -0800

    pq

---
+++ LeetcodeProblems/FB/LC23MergeKSortedLists.java
@@ -0,0 +1,31 @@
+package FB;
+
+import java.util.PriorityQueue;
+
+public class LC23MergeKSortedLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0 || lists == null) return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for (ListNode list: lists) {
+            if (list != null) pq.offer(list);
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            ListNode tmp = pq.poll();
+            cur.next = tmp;
+            cur = cur.next;
+            if (tmp.next != null) {
+                pq.offer(tmp.next);
+            }
+        }
+        return dummy.next;
+    }
+}
