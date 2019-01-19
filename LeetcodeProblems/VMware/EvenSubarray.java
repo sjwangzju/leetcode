db12b4e9e454e862b82111acdeb3f2cf32e2cbad
sjwang <sjwang0719@gmail.com>
2019-01-18 16:12:21 -0800

    subarray

--- LeetcodeProblems/VMware/EvenSubarray.java
+++ LeetcodeProblems/VMware/EvenSubarray.java
@@ -3,25 +3,21 @@ package VMware;
 import java.util.*;
 
 public class EvenSubarray {
-    public Set<List<Integer>> findSub(int[] input, int k) {
+
+    public int evenSubarray(int[] input, int k) {
         int l = 0;
         int r = 0;
         int cnt = 0;
         Set<List<Integer>> res = new HashSet<>();
+
         while (l <= r && r < input.length) {
-            if (isOdd(input[r])) {
+            if (input[r] % 2 == 1) {
                 cnt++;
             }
-            if (cnt == k) {
-                add(res, l, r, input);
-                while (l < r) {
-                    add(res, l, r, input);
-                    int tmp = r + 1;
-                    while (tmp < input.length && !isOdd(input[tmp])) {
-                        add(res, l, tmp, input);
-                        tmp++;
-                    }
-                    if (isOdd(input[l])) break;
+            if (cnt <= k) {
+                getSubArray(res, l, r, input);
+            } else {
+                while (l <= r && input[l] % 2 == 0) {
                     l++;
                 }
                 l++;
@@ -29,29 +25,23 @@ public class EvenSubarray {
             }
             r++;
         }
-        return res;
+        return res.size();
     }
 
-    public boolean isOdd(int n) {
-        return n % 2 == 1;
-    }
-
-    public void add(Set<List<Integer>> res, int l, int r, int[] input) {
-        List<Integer> cur = new LinkedList<>();
+    public void getSubArray(Set<List<Integer>> res, int l, int r, int[] input) {
+        List<Integer> cur;
         for (int i = l; i <= r; i++) {
-            cur.add(input[i]);
+            cur = new LinkedList<>();
+            for (int j = i; j <= r; j++) {
+                cur.add(input[j]);
+                res.add(new ArrayList<>(cur));
+            }
         }
-        res.add(cur);
     }
+
 
     public static void main(String[] args) {
-        int[] input = {2,5,6,9};
-        Set<List<Integer>> res = new EvenSubarray().findSub(input, 2);
-        for (List<Integer> list: res) {
-            for (int n: list) {
-                System.out.print(n + " ");
-            }
-            System.out.println();
-        }
+        int[] input = {1,2,3,4};
+        System.out.println(new EvenSubarray().evenSubarray(input, 1));
     }
 }
