9d9478f08474671c910af57e8e4d2671dc69dde1
sjwang <sjwang0719@gmail.com>
2019-01-21 15:17:56 -0800

    union find

---
+++ LeetcodeProblems/FB/LC785BipartiteGraph.java
@@ -0,0 +1,45 @@
+package FB;
+
+
+public class LC785BipartiteGraph {
+
+    /**
+     * union find
+     *
+     * time: O(
+     *
+     * @param graph
+     * @return
+     */
+    public boolean isBipartite(int[][] graph) {
+        int[] parent = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < graph.length; i++) {
+            int[] tmp = graph[i];
+            if (tmp.length == 0 || tmp == null) continue;
+            int p1 = findParent(parent, i);
+            int pp = findParent(parent, tmp[0]);
+
+            for (int t: tmp) {
+                int p2 = findParent(parent, t);
+                if (p1 == p2) return false;
+                parent[p2] = pp;
+            }
+        }
+        return true;
+    }
+
+    public int findParent(int[] parent, int n) {
+        while (n != parent[n]) {
+            n = parent[n];
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {{1,4},{0,2},{1},{4},{0,3}};
+        System.out.println(new LC785BipartiteGraph().isBipartite(graph));
+    }
+}
