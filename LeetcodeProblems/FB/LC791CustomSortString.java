67ba3aef62963ef97ddf5a1d5ebe3d1c3c93e674
sjwang <sjwang0719@gmail.com>
2019-01-21 22:06:01 -0800

    sort

--- LeetcodeProblems/FB/LC791CustomSortString.java
+++ LeetcodeProblems/FB/LC791CustomSortString.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class LC791CustomSortString {
 
     /**
-     * time: O(nlogn), space: O(n)
+     * time: O(MlogM), space: O(M), M - len of T
      *
      * @param S
      * @param T
      * @return
      */
-    public String customSortString(String S, String T) {
+    public String customSortStringI(String S, String T) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < S.length(); i++) {
             map.put(S.charAt(i), i);
@@ -29,9 +29,42 @@ public class LC791CustomSortString {
         return res;
     }
 
+    /**
+     * time: O(N + M), space: O(1)
+     * @param S
+     * @param T
+     * @return
+     */
+    public String customSortStringII(String S, String T) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < T.length(); i++) {
+            char ch = T.charAt(i);
+            freq[ch - 'a']++;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            while (freq[ch - 'a'] > 0) {
+                res.append(ch);
+                freq[ch - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0) {
+                res.append((char)('a' + i));
+                freq[i]--;
+            }
+        }
+        return res.toString();
+    }
+
+
     public static void main(String[] args) {
         String S = "cba";
         String T = "abcd";
-        System.out.println(new LC791CustomSortString().customSortString(S, T));
+        System.out.println(new LC791CustomSortString().customSortStringII(S, T));
     }
 }
