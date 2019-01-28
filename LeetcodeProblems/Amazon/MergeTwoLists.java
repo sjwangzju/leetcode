c0de3f8c8179687edf790937b29c34470498c007
sjwang <sjwang0719@gmail.com>
2019-01-27 21:53:34 -0800

    recursion

---
+++ LeetcodeProblems/Amazon/MergeTwoLists.java
@@ -0,0 +1,52 @@
+package Amazon;
+
+public class MergeTwoLists {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            this.val = x;
+        }
+    }
+
+    /**
+     * recursion
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeTwoLists(l2.next, l1);
+        return l2;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l12 = new ListNode(2);
+        ListNode l13 = new ListNode(4);
+        l1.next = l12;
+        l12.next = l13;
+
+        ListNode l2 = new ListNode(1);
+        ListNode l22 = new ListNode(3);
+        ListNode l23 = new ListNode(4);
+        l2.next = l22;
+        l22.next = l23;
+
+        ListNode res = new MergeTwoLists().mergeTwoLists(l1, l2);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+}
