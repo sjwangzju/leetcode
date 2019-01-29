c57ac62f47ba7350cd7d07d897b9b03a66adaa5f
sjwang <sjwang0719@gmail.com>
2019-01-28 19:49:40 -0800

    recursion

--- LeetcodeProblems/Amazon/MaximumAverageNode.java
+++ LeetcodeProblems/Amazon/MaximumAverageNode.java
@@ -14,15 +14,15 @@ public class MaximumAverageNode {
         }
     }
 
-    public int maximumAverage(CategoryNode root) {
-        int[] res = new int[1];
+    public CategoryNode maximumAverage(CategoryNode root) {
+        CategoryNode[] res = new CategoryNode[1];
         double[] max = new double[1];
         max[0] = Double.MIN_VALUE;
         search(root, max, res);
         return res[0];
     }
 
-    public int[] search(CategoryNode root, double[] max, int[] res) {
+    public int[] search(CategoryNode root, double[] max, CategoryNode[] res) {
         if (root == null) return new int[2];
         int[] n = new int[2];
         n[0] = root.value;
@@ -31,36 +31,30 @@ public class MaximumAverageNode {
             n[0] += search(c, max, res)[0];
             n[1] += search(c, max, res)[1];
         }
-        if (n[1] > 1) {
-            if ((double)n[0] / n[1] > max[0]) {
-                max[0] = (double)n[0] / n[1];
-                res[0] = root.value;
-            }
+        if (n[1] > 1 && (double) (n[0] / n[1]) > max[0]) {
+            max[0] = (double) (n[0] / n[1]);
+            res[0] = root;
         }
         return n;
     }
 
     public static void main(String[] args) {
-        CategoryNode n1 = new CategoryNode(20);
-
-        CategoryNode n21 = new CategoryNode(12);
-        CategoryNode n22 = new CategoryNode(18);
-
-        CategoryNode n31 = new CategoryNode(11);
-        CategoryNode n32 = new CategoryNode(2);
+        CategoryNode n1 = new CategoryNode(5);
+        CategoryNode n21 = new CategoryNode(6);
+        CategoryNode n22 = new CategoryNode(8);
+        CategoryNode n31 = new CategoryNode(7);
+        CategoryNode n32 = new CategoryNode(10);
         CategoryNode n33 = new CategoryNode(3);
-        CategoryNode n34 = new CategoryNode(15);
-        CategoryNode n35 = new CategoryNode(8);
+        CategoryNode n34 = new CategoryNode(1);
 
         n1.subCategoryNode.add(n21);
         n1.subCategoryNode.add(n22);
         n21.subCategoryNode.add(n31);
         n21.subCategoryNode.add(n32);
-        n21.subCategoryNode.add(n33);
+        n22.subCategoryNode.add(n33);
         n22.subCategoryNode.add(n34);
-        n22.subCategoryNode.add(n35);
 
-        int res = new MaximumAverageNode().maximumAverage(n1);
-        System.out.println(res);
+        CategoryNode res = new MaximumAverageNode().maximumAverage(n1);
+        System.out.println(res.value);
     }
 }
