873e75b9e291b1954ca1900815479102a4ffa7df
sjwang <sjwang0719@gmail.com>
2019-01-29 12:48:21 -0800

    kruskal

--- LeetcodeProblems/Amazon/MST.java
+++ LeetcodeProblems/Amazon/MST.java
@@ -1,6 +1,12 @@
 package Amazon;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class MST {
 
