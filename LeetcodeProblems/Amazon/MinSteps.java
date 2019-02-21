f4fafbd4e77c69415614fc7ecd90aa93b4f76580
sjwang <sjwang0719@gmail.com>
2019-02-20 21:05:45 -0800

    dfs

---
+++ LeetcodeProblems/Amazon/MinSteps.java
@@ -0,0 +1,20 @@
+package Amazon;
+
+public class MinSteps {
+
+    public int findMinStep(int n) {
+        return dfs(n);
+    }
+
+    public int dfs(int n) {
+        if (n == 1) return 0;
+        if (n % 2 == 1) {
+            return 1 + Math.min(dfs(n - 1), dfs(n + 1));
+        }
+        return 1 + dfs(n / 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MinSteps().findMinStep(5));
+    }
+}
