eb239fe8e94935b299c8bef0d4458bb6d07769d9
sjwang <sijia.wang@sv.cmu.edu>
2019-05-26 14:46:09 -0700

    num of meeting rooms

--- LeetcodeProblems/FullTime/FB/LC353_MeetingRoomsII_M.java
+++ LeetcodeProblems/FullTime/FB/LC353_MeetingRoomsII_M.java
@@ -1,12 +1,38 @@
-package FB;
+package FullTime.FB;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+
+/**
+ * time: O(N), N is the num of intervals
+ *
+ * space: O(N)
+ */
 public class LC353_MeetingRoomsII_M {
 
     public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
 
+        Arrays.sort(intervals, (int[] a, int[] b) -> (a[0] - b[0]));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(intervals[0][1]);
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] >= pq.peek()) {
+                pq.poll();
+            }
+
+            pq.offer(intervals[i][1]);
+        }
+
+        return pq.size();
     }
 
     public static void main(String[] args) {
-
+        int[][] intervals = {{0,30}, {5,10}, {15,20}};
+        System.out.println(new LC353_MeetingRoomsII_M().minMeetingRooms(intervals));
     }
 }
