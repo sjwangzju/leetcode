ae7dc1d95fb6d6c73e2e20dc0ad45146773d1b25
sjwang <sijia.wang@sv.cmu.edu>
2019-05-27 15:01:54 -0700

    in-order traversal

---
+++ LeetcodeProblems/FullTime/FB/LC426_ConvertBinarySearchTreeToSortedDoublyLinkedList.java
@@ -0,0 +1,66 @@
+package FullTime.FB;
+
+/**
+ * BST in-place transform to doubly-linked list
+ *
+ * -> in-order traversal
+ *
+ * time: O(N), N is the num of nodes in BST
+ * space: O(N), depth of the recursive call stack -> best: O(logN), worst: O(N)
+ *
+ */
+public class LC426_ConvertBinarySearchTreeToSortedDoublyLinkedList {
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+
+    Node first = null;
+    Node last = null;
+
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+
+        DFS_inorder(root);
+        last.right = first;
+        first.left = last;
+        return first;
+    }
+
+    public void DFS_inorder(Node cur) {
+        if (cur == null) return;
+
+        // in-order traversal
+        DFS_inorder(cur.left);
+
+        if (first == null) first = cur;
+        if (last != null) {
+            last.right = cur;
+            cur.left = last;
+        }
+        last = cur;
+
+        DFS_inorder(cur.right);
+    }
+
+    public static void main(String[] args) {
+        Node n31 = new Node(1, null, null);
+        Node n32 = new Node(3, null, null);
+        Node n21 = new Node(2, n31, n32);
+        Node n22 = new Node(5, null, null);
+        Node n1 = new Node(4, n21, n22);
+
+        Node tmp = new LC426_ConvertBinarySearchTreeToSortedDoublyLinkedList().treeToDoublyList(n1);
+        System.out.println(tmp.val);
+    }
+}
