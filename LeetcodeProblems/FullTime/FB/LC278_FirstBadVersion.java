318dd5a50a561415dae12103b6c22168e9b36221
sjwang <sijia.wang@sv.cmu.edu>
2019-05-30 21:14:11 -0700

    binary search

---
+++ LeetcodeProblems/FullTime/FB/LC278_FirstBadVersion.java
@@ -0,0 +1,33 @@
+package FullTime.FB;
+
+/**
+ * binary search
+ *
+ * time: O(logN)
+ * space: O(1)
+ */
+public class LC278_FirstBadVersion {
+
+    public int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (!isBadVersion(mid)) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+
+    public boolean isBadVersion(int n) {
+        return n >= 4;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC278_FirstBadVersion().firstBadVersion(5));
+    }
+}
