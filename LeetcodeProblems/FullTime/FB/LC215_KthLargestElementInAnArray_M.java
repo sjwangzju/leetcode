8d9e746030c9afff90cf825d3f9a8596629075b1
sjwang <sijia.wang@sv.cmu.edu>
2019-06-06 23:02:14 -0700

    quick select

--- LeetcodeProblems/FullTime/FB/LC215_KthLargestElementInAnArray_M.java
+++ LeetcodeProblems/FullTime/FB/LC215_KthLargestElementInAnArray_M.java
@@ -28,64 +28,42 @@ public class LC215_KthLargestElementInAnArray_M {
     // quick select
     // time: avg - O(N), worst - O(N2)
     // space: O(1)
-    int [] nums;
-
-    public void swap(int a, int b) {
-        int tmp = this.nums[a];
-        this.nums[a] = this.nums[b];
-        this.nums[b] = tmp;
+    public int findKthLargestII(int[] nums, int k) {
+        int len = nums.length;
+        return quickSelect(nums, 0, len - 1, len - k);
     }
 
+    public int quickSelect(int[] nums, int lo, int hi, int k) {
+        if (lo == hi) return nums[lo];
 
-    public int partition(int left, int right, int pivot_index) {
-        int pivot = this.nums[pivot_index];
-        // 1. move pivot to end
-        swap(pivot_index, right);
-        int store_index = left;
+        Random random = new Random();
+        int pivotIndex = lo + random.nextInt(hi - lo);
+        int curPivot = partition(nums, lo, hi, pivotIndex);
 
-        // 2. move all smaller elements to the left
-        for (int i = left; i <= right; i++) {
-            if (this.nums[i] < pivot) {
-                swap(store_index, i);
-                store_index++;
+        if (curPivot == k) return nums[curPivot];
+        if (curPivot > k) return quickSelect(nums, lo, curPivot - 1, k);
+        return quickSelect(nums, curPivot + 1, hi, k);
+    }
+
+    public int partition(int[] nums, int lo, int hi, int pivotIndex) {
+        int pos = lo;
+        int pivot = nums[pivotIndex];
+        swap(nums, pivotIndex, hi);
+
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] < pivot) {
+                swap(nums, i, pos);
+                pos++;
             }
         }
-
-        // 3. move pivot to its final place
-        swap(store_index, right);
-
-        return store_index;
+        swap(nums, pos, hi);
+        return pos;
     }
 
-    public int quickselect(int left, int right, int k_smallest) {
-    /*
-    Returns the k-th smallest element of list within left..right.
-    */
-
-        if (left == right) // If the list contains only one element,
-            return this.nums[left];  // return that element
-
-        // select a random pivot_index
-        Random random_num = new Random();
-        int pivot_index = left + random_num.nextInt(right - left);
-
-        pivot_index = partition(left, right, pivot_index);
-
-        // the pivot is on (N - k)th smallest position
-        if (k_smallest == pivot_index)
-            return this.nums[k_smallest];
-            // go left side
-        else if (k_smallest < pivot_index)
-            return quickselect(left, pivot_index - 1, k_smallest);
-        // go right side
-        return quickselect(pivot_index + 1, right, k_smallest);
-    }
-
-    public int findKthLargestII(int[] nums, int k) {
-        this.nums = nums;
-        int size = nums.length;
-        // kth largest is (N - k)th smallest
-        return quickselect(0, size - 1, size - k);
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
     public static void main(String[] args) {
