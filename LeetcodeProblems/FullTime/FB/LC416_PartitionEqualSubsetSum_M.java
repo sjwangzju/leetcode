47e744d8cc53c328c01e923c52a39e360906a62f
sjwang <sijia.wang@sv.cmu.edu>
2019-06-26 22:56:42 -0700

    backtracking + memory

---
+++ LeetcodeProblems/FullTime/FB/LC416_PartitionEqualSubsetSum_M.java
@@ -0,0 +1,46 @@
+package FullTime.FB;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Backtracking with memory - Use Set for memorization
+ *
+ * time: O(2^N)
+ * space: O(2^N)
+ */
+public class LC416_PartitionEqualSubsetSum_M {
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int n: nums) {
+            sum += n;
+        }
+        if (sum % 2 == 1) return false;
+
+        return backtracking(sum, nums, 0, 0, new HashSet<>());
+    }
+
+    public boolean backtracking(int sum, int[] nums, int cur, int i, Set<Integer> invalid) {
+        if (cur == sum / 2) {
+            return true;
+        }
+        if (invalid.contains(cur) || i > nums.length || cur > sum / 2) {
+            return false;
+        }
+        invalid.add(cur);
+        for (int j = i; j < nums.length; j++) {
+            cur += nums[j];
+            if (backtracking(sum, nums, cur, j + 1, invalid)) {
+                return true;
+            }
+            cur -= nums[j];
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,2};
+        System.out.println(new LC416_PartitionEqualSubsetSum_M().canPartition(nums));
+    }
+}
