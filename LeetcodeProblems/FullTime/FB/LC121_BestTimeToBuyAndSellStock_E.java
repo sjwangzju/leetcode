8f101819297216e64f65f8d95829e7e0a05d84fe
sjwang <sijia.wang@sv.cmu.edu>
2019-06-27 23:18:54 -0700

    two solutions

--- LeetcodeProblems/FullTime/FB/LC121_BestTimeToBuyAndSellStock_E.java
+++ LeetcodeProblems/FullTime/FB/LC121_BestTimeToBuyAndSellStock_E.java
@@ -1,16 +1,13 @@
 package FullTime.FB;
 
-/**
- * left[] for min from left & right[] for max from right
- *
- * Max(right[i] - left[i])
- *
- * time: O(N)
- * space: O(N)
- */
 public class LC121_BestTimeToBuyAndSellStock_E {
 
-    public int maxProfit(int[] prices) {
+    //left[] for min from left & right[] for max from right
+    // Max(right[i] - left[i])
+    //
+    // time: O(N)
+    // space: O(N)
+    public int maxProfitI(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
         int l = prices.length;
         int[] left = new int[l];
@@ -35,8 +32,25 @@ public class LC121_BestTimeToBuyAndSellStock_E {
         return res;
     }
 
+    // time: O(N)
+    // space: O(1)
+    public int maxProfitII(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int minPrice = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int n: prices) {
+            if (n < minPrice) {
+                minPrice = n;
+            } else if (n - minPrice > max) {
+                max = n - minPrice;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
-        System.out.println(new LC121_BestTimeToBuyAndSellStock_E().maxProfit(prices));
+        System.out.println(new LC121_BestTimeToBuyAndSellStock_E().maxProfitII(prices));
     }
 }
