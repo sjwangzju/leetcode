d8405c9de12c288b761e223c17738ba52b9b87fb
sjwang <sijia.wang@sv.cmu.edu>
2019-06-30 10:43:08 -0700

    dp

--- LeetcodeProblems/FullTime/FB/LC1105_FillingBookcaseShelves_M.java
+++ LeetcodeProblems/FullTime/FB/LC1105_FillingBookcaseShelves_M.java
@@ -1,36 +1,31 @@
 package FullTime.FB;
 
 /**
- * DFS + memory
+ * DP
  *
- * time: O(N)
- * space: O(N*W), N is num of books, W is shelf-width
+ * time: O(N^2)
+ * space: O(N)
  */
 public class LC1105_FillingBookcaseShelves_M {
 
-    int[][] dp;
     public int minHeightShelves(int[][] books, int shelf_width) {
-        dp = new int[books.length + 1][shelf_width + 1];
-        return minHeight(books, shelf_width, 0, 0, shelf_width);
-    }
+        int[] dp = new int[books.length];
 
-    public int minHeight(int[][] books, int widthLeft, int i, int curHeight, int shelf_width) {
-        if (i == books.length) {
-            return curHeight;
-        }
-        if (dp[i][widthLeft] != 0) {
-            return dp[i][widthLeft];
-        }
+        for (int i = 0; i < books.length; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            int curWidth = 0;
+            int curMaxHeight = Integer.MIN_VALUE;
 
-        // put on a new shelf
-        dp[i][widthLeft] = curHeight + minHeight(books, shelf_width - books[i][0], i + 1, books[i][1], shelf_width);
+            for (int j = i; j >= 0; j--) {
+                curWidth += books[j][0];
+                curMaxHeight = Math.max(curMaxHeight, books[j][1]);
+                if (curWidth > shelf_width) break;
 
-        // put on the prev shelf
-        if (widthLeft >= books[i][0]) {
-            int h = minHeight(books, widthLeft - books[i][0], i + 1, Math.max(curHeight, books[i][1]), shelf_width);
-            dp[i][widthLeft] = Math.min(dp[i][widthLeft], h);
+                int curH = curMaxHeight + (j >= 1? dp[j - 1]: 0);
+                dp[i] = Math.min(dp[i], curH);
+            }
         }
-        return dp[i][widthLeft];
+        return dp[books.length - 1];
     }
 
     public static void main(String[] args) {
